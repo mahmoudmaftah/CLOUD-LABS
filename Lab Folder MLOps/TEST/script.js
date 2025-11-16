d5f6aeaa2b330167164500485e337ec620f3975c
Maftah Mahmoud <mahmoudmaftah44@gmail.com>
2025-11-16 23:54:43 +0100

    LAB2

---
+++ Lab Folder MLOps/TEST/script.js
@@ -0,0 +1,13 @@
+
+      $('.order').click(function (e) {
+
+    let button = $(this);
+  
+    if (!button.hasClass('animate')) {
+      button.addClass('animate');
+      setTimeout(() => {
+        button.removeClass('animate');
+      }, 10000);
+    }
+  
+});
